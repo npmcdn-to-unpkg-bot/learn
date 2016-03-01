11fb44b567bb5f265a4870865c0d7f196a74937f
Tao Zhi <taozhiwork@foxmail.com>
2016-03-01 14:44:25 +0800

    update

---
+++ weui/gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var minifyCss = require("gulp-minify-css");
+var rename = require("gulp-rename");
+var sourcemaps = require("gulp-sourcemaps");
+var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+
+gulp.task('sass',function() {
+    var processors = [
+        autoprefixer()
+    ];
+
+    gulp.src('./sass/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss(processors))
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".css"
+        }))
+        .pipe(minifyCss())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./style'));
+});
+
+gulp.task('sass:watch',function() {
+    gulp.watch('./sass/*.scss', ['sass']);
+});
+
+gulp.task('default',['sass','sass:watch']);
+
