d0c3aa1e49c715bdad9b0fc4cc5b01bd5741ba05
Tao Zhi <taozhiwork@foxmail.com>
2016-03-02 18:27:40 +0800

    update

--- weui/script/main.js
+++ weui/script/main.js
@@ -1,5 +1,18 @@
 $(function(){
-    $('#button').on('tap',function(){
-        alert('ok');
+
+    // 注册
+    $('#hub_register').on('tap',function(){
+        var $form = $('#hub_register_form'),
+            $requireds = $form.find('input[required]');
+
+        for(var i = 0,len = $requireds.length; i < len; i++){
+            var $input = $requireds.eq(i),
+                inputName;
+            if($.trim($input.val()) === ''){
+                inputName = $input.attr('name');
+                alert('必填项不能为空！')
+                return false;
+            }
+        }
     });
 });
