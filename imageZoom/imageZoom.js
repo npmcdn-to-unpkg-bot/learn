fbf7ba78cac4b2cde94b624566c62d2ed72def47
Tao Zhi <taozhiwork@foxmail.com>
2016-03-08 09:58:17 +0800

    update

--- imageZoom/imageZoom.js
+++ imageZoom/imageZoom.js
@@ -48,8 +48,7 @@
             right: 0,
             bottom: 0,
             left: 0,
-            backgroundColor: '#000',
-            backgroundColor: 'rgba(0,0,0,.7)',
+            backgroundColor: '#222',
             zIndex: 9998,
             filter: 'alpha(opcity=70)'
         });
