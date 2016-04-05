87f14fae9aba0243df3e98944dfba6442525ebc1
taozhi <taozhi1028@gmail.com>
2016-04-05 17:52:56 +0800

    update

--- angular2-tour-of-heroes/app/hero.ts
+++ angular2-tour-of-heroes/app/hero.ts
@@ -1,7 +1,11 @@
-/**
- * Created by xiaohu on 2016/4/5.
- */
 export class Hero {
-    id: number;
-    name: string;
+	id: number;
+	name: string;
 }
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
