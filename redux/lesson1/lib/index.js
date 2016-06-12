3381216700ab57986e060412fa841c2db4482e62
taozhi <taozhi1028@gmail.com>
2016-06-12 18:21:49 +0800

    update

---
+++ redux/lesson1/lib/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'redux';
+
+// reducer
+function counter(state = 0, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+// store
+const store = createStore(counter);
